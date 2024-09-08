7de61447ad892ac37b148b45babbe214121c85f7
CJ Mahdi <125257765+cjmahdi2@users.noreply.github.com>
2024-09-08 15:32:41 +0330

    Update displayUtils.js

--- src/displayUtils.js
+++ src/displayUtils.js
@@ -4,8 +4,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=           Sonic Odyssey BOT          ='.cyan);
-  console.log('=     Created by HappyCuanAirdrop      ='.cyan);
-  console.log('=    https://t.me/HappyCuanAirdrop     ='.cyan);
+  console.log('=           Created by CJMahdi         ='.cyan);
+  console.log('=    https://t.me/cjmahdi              ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
